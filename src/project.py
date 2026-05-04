abb28a85079f936e344e5f723affc63a70eab095
Nicole Le <lenicole989@gmail.com>
2026-05-04 15:27:50 -0500

    Initialize function, establish position, size, and speed of character

--- src/project.py
+++ src/project.py
@@ -1,12 +1,23 @@
 import pygame
+import random
+import math
+import sys
 
+blue = (0, 81, 186)
 
-def main():
+class character:
+    def __init__(self, x, y):
+        # Store coordinates
+        self.pos = [x, y]
+        self.size = [30, 20]
+        self.speed = 5
+        screen = pygame.display.set_mode((800, 500))
+        pygame.display.set_caption("Shopping at 1KEA")
 
+clock = pygame.time.Clock()
+player_position = [400, 250]
 
-
-
-
+while True:
 
 
 
